cfd44a5954544f4acf179d5e1c1177061d01b6d4
kishan <kishan.bind_cs18@gla.ac.in>
2020-02-19 22:20:34 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h1 = sc.nextInt();
+        int h2 = sc.nextInt();
+        int h3 = sc.nextInt();
+
+        boolean descOrdered =( (h1 == h2) || (h2 == h3));
+
+        System.out.println(descOrdered);
+    }
+}
